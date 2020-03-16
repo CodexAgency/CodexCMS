a5f2e3a8f029d4b0ddb4150a26751b1b2128e5b5
Carmine Milieni <carmine.milieni@gmail.com>
2020-03-16 14:41:29 +0100

    Primo up

---
+++ app/Controllers/Backend/Auth/Login.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Controllers\Backend\Auth;
+
+use App\Controllers\Backend\Auth\AuthController;
+
+class Login extends AuthController
+{
+
+    /**
+     * An array of helpers to be loaded automatically upon
+     * class instantiation. These helpers will be available
+     * to all other controllers that extend BaseController.
+     *
+     * @var array
+     */
+    protected $helpers = [];
+
+    /**
+     * Constructor.
+     */
+    public function initController(\CodeIgniter\HTTP\RequestInterface $request, \CodeIgniter\HTTP\ResponseInterface $response, \Psr\Log\LoggerInterface $logger)
+    {
+        // Do Not Edit This Line
+        parent::initController($request, $response, $logger);
+
+        //--------------------------------------------------------------------
+        // Preload any models, libraries, etc, here.
+        //--------------------------------------------------------------------
+        // E.g.:
+        // $this->session = \Config\Services::session();
+    }
+
+    public function index()
+    {
+        return view('backend/layout');
+    }
+}
